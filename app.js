edf64a4af0ebcd328a748a5f298e76c79575f59b
John Berry <ulfmagnetics@gmail.com>
2016-10-24 00:28:28 -0400

    Move required CSS and JS to public directory
    Someday I can automate all this with Grunt, but not today

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
 
 var port = process.env.PORT || 6565;
 app.listen(port, function () {
