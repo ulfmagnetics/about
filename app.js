cf606d3ff6bc7ba74f3da115d46bed516621dc37
John Berry <ulfmagnetics@gmail.com>
2016-10-23 21:22:47 -0400

    Create a 'Hello World' Bootstrap 3 site

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/bower_components', express.static(path.join(__dirname, '/bower_components')));
+
+var port = 6565;
+app.listen(port, function () {
+  console.log('Dev server listening on port ' + port + '!');
+});
